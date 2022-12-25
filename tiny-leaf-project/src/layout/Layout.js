9480b2427b9bee75f311c3a1a3925e1399f8e724
Emeka1993 <brolin933@gmail.com>
2022-12-25 21:04:48 +0000

    commit

--- tiny-leaf-project/src/layout/Layout.js
+++ tiny-leaf-project/src/layout/Layout.js
@@ -1,22 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
+import React from 'react';
+import './leaf.css';
+import './Nav.css';
 
 const Layout = () => {
   return (
-  
     <>
-    {/* //header */}
-    <header style={{ backgroundColor: '#86907D', padding: '40px' 
-
-  
-  }}>...</header> 
-
+    <header style={{ backgroundColor: '#86907D', padding: '40px' }}>...</header> 
     <header>
     <nav>
-        
-
+    <nav className="right-align">
+      {/* Nav content goes here */}
+    </nav>
         <ul>
           <li>
-            <Link to="/Home">Home</Link>
+           <Link to="/Home">Home</Link>
           </li>
           <li>
             <Link to="/Menu">Menu</Link>
