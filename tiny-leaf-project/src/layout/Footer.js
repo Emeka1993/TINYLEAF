eac139ee669d663b3a671fd0d304481894598fdf
Emeka1993 <brolin933@gmail.com>
2023-01-29 23:01:47 +0000

    footer

--- tiny-leaf-project/src/layout/Footer.js
+++ tiny-leaf-project/src/layout/Footer.js
@@ -6,20 +6,20 @@ const Footer = () => {
       <img src={require('../images/newlogo.png')} style={{float: 'left', height: '50px'}} alt="Logo" />
       <table>
         <tr>
-          <th className="header-cell1">Opening hours</th>
-          <th className="header-cell2">Address</th>
-          <th className="header-cell3">Contact</th>
+          <th className="opening-hours">Opening hours</th>
+          <th className="Address">Address</th>
+          <th className="Contact">Contact</th>
         </tr>
         <tr>
-          <td className="cell1">
+          <td className="hours">
             Mon - Sat 11am - 12pm <br />
             Sun 11am - 11pm
           </td>
-          <td className="cell2">
+          <td className="main-address">
             24 Brew Lane <br />
             BR3 WL Cuptown
           </td>
-          <td className="cell3">
+          <td className="contact-us">
             +44 201 998 7562 <br />
             hello@yellowmountain.com
           </td>
